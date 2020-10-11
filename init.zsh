18fa1a5df361f46173a85ad42cb1c3c672d359a4
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-10-11 16:30:25 -0400

    fix(api): drop shell:: now that its split, depends on `p6m7g8/p6shell`

--- init.zsh
+++ init.zsh
@@ -1,20 +1,33 @@
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::version()
+# Function: p6df::modules::irc::version()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::version() { echo "0.0.1" }
+p6df::modules::irc::version() { echo "0.0.1" }
 
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::external::brew()
+# Function: p6df::modules::irc::deps()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::external::brew() {
+p6df::modules::irc::deps() {
+  ModuleDeps=(
+    p6m7g8/p6df-shell
+  )
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::irc::external::brew()
+#
+#>
+######################################################################
+p6df::modules::irc::external::brew() {
 
   brew install irssi
 }
@@ -22,11 +35,11 @@ p6df::modules::shell::irc::external::brew() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::home::symlink()
+# Function: p6df::modules::irc::home::symlink()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::home::symlink() {
+p6df::modules::irc::home::symlink() {
 
   ln -fs $P6_DFZ_SRC_P6M7G8_DIR/p6df-irc/share/irssi .irssi
 }
@@ -34,37 +47,37 @@ p6df::modules::shell::irc::home::symlink() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::init()
+# Function: p6df::modules::irc::init()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::init() {
+p6df::modules::irc::init() {
 
-  p6df::modules::shell::irc::aliases::init
+  p6df::modules::irc::aliases::init
 }
 
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::aliases::init()
+# Function: p6df::modules::irc::aliases::init()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::aliases::init() {
+p6df::modules::irc::aliases::init() {
 
-  alias irc='p6df::modules::shell::irc'
-  alias irc_attach='p6df::modules::shell::irc::attach'
-  alias irc_init='p6df::modules::shell::irc::init'
+  alias irc='p6df::modules::irc'
+  alias irc_attach='p6df::modules::irc::attach'
+  alias irc_init='p6df::modules::irc::init'
 }
 
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::attach()
+# Function: p6df::modules::irc::attach()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::attach() {
+p6df::modules::irc::attach() {
 
   tmux attach -t irc
 }
@@ -72,11 +85,11 @@ p6df::modules::shell::irc::attach() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc::init()
+# Function: p6df::modules::irc::start()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc::init() {
+p6df::modules::irc::start() {
 
   tmux new -s irc irssi
 }
@@ -84,11 +97,11 @@ p6df::modules::shell::irc::init() {
 ######################################################################
 #<
 #
-# Function: p6df::modules::shell::irc()
+# Function: p6df::modules::irc()
 #
 #>
 ######################################################################
-p6df::modules::shell::irc() {
+p6df::modules::irc() {
 
   if ! irc_attach ; then
     irc_init
